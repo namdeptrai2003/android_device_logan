4664216a734850812ac8776f525183e5070f98a7
Real man born on semtember <hieunam03@gmail.com>
2017-02-18 19:06:07 +0700

    Update and rename cm.mk to lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,6 +1,3 @@
-# Release name
-PRODUCT_RELEASE_NAME := GT-S7270
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
@@ -9,7 +6,7 @@ $(call inherit-product, device/samsung/logan/device_logan.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := logan
-PRODUCT_NAME := cm_logan
+PRODUCT_NAME := lineage_logan
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-S7270
